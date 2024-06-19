ea9d71f2856017774d11bcd46dff62ee05d4f1af
Hitesh <duragiyah@gmail.com>
2024-06-19 07:25:15 +0530

    Add project files.

---
+++ TestDB/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using FinanceReport.Models.POCO;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TestDB.Models.Binder;
+
+namespace TestDB
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinders.Binders.Add(typeof(ExpenseCategory), new ExpenseCategoryModelBinder());
+        }
+    }
+}
---
+++ TestDB/Models/Binder/ExpenseCategoryModelBinder.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Register the custom model binder in the Application_Start method gloabl.aspx
+ * Use the custom model binder in your controller actions
+ */
+using FinanceReport.Models.POCO;
+using System.Web.Mvc;
+
+namespace TestDB.Models.Binder
+{
+    public class ExpenseCategoryModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var request = controllerContext.HttpContext.Request;
+
+            var categoryIdValue = request.Form.Get("inputId");
+            var categoryNameValue = request.Form.Get("inputText");
+
+            int.TryParse(categoryIdValue, out var categoryId);
+
+            return new ExpenseCategory
+            {
+                ExpenseCategoryId = categoryId,
+                ExpenseCategoryName = categoryNameValue
+            };
+        }
+    }
+}
